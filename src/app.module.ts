473cd6bdfc2a6205410331a49ba75c22ab0d9160
Orlin Alvarado <oalvaradomorales@gmail.com>
2022-11-02 13:42:43 +0000

    Seccion 5 finalizada

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USESRNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
